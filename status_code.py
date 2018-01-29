072f194999e8f6d95563c0ef7d824fd3ca34ee5c
Akanksha verma <ruhi461@gmail.com>
2018-01-29 14:59:08 +0530

    Update status_code.py

--- status_code.py
+++ status_code.py
@@ -1,41 +1,21 @@
 import requests
-from selenium import webdriver
-import time
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import json
 
-chromepath = "/home/aj/Downloads/chromedriver"
-driver=webdriver.Chrome(chromepath)
-# chrome_options = Options()
-# chrome_options.add_extension("/home/aj/Downloads/way2sms_v3.8.crx")
-# DesiredCapabilities.desiredcapabilities=DesiredCapabilities.CHROME()
-# DesiredCapabilities.setCapability(chrome_options.CAPABILITY, chrome_options)
-# driver= webdriver.Chrome(desired_capabilities=DesiredCapabilities)
-# # driver = webdriver.Firefox(capabilities=capabilities)
-# #
-# # 		DesiredCapabilities desiredcapabilities=DesiredCapabilities.chrome();
-# # 		desiredcapabilities.setCapability(chromeoption.CAPABILITY, chromeoption);
-# # 		driver=new ChromeDriver(desiredcapabilities);
-driver.get("http://lotteryadmin.sia.co.in/#/login")
-r = requests.get("http://lotteryadmin.sia.co.in/#/login")
-print (r.status_code)
-
-if r.status_code ==100:
-    print("server is working fine")
-
+# Check status code for website.
+response = requests.get("url of the website")
+print(response.status_code)
+if response.status_code ==200:
+      print("server is working fine")
+  
+else:
+	response2=requests.get("http://www.kit19.com/ComposeSMS.aspx?username=id&password=password&sender=senderid&to=mobileno&message=Their%20is%20some%20technical%20issue%20in%20lottery%20admin%20pannel%20please%20check%20it%20immediately&priority=1&dnd=1&unicode=0")
+ 
+# Check status code for another website".	
+response3 = requests.get("url of second wesite")
+print(response3.status_code)
+if response3.status_code ==200:
+      print("server is working fine")
 
 else:
-    driver.get("http://site21.way2sms.com/content/index.html?")
-    driver.find_element_by_id("username").send_keys("8591771290")
-    driver.find_element_by_id("password").send_keys("P5879N")
-    driver.find_element_by_id("loginBTN").click()
-    driver.find_element_by_id("sendSMS").click()
-    driver.switch_to.frame("frame")
-    driver.find_element_by_id("mobile").send_keys("8591771290")
-    driver.find_element_by_id("message").send_keys("hi")
-    time.sleep (4)
-    # driver.find_element_by_id("Send").click()
-    # time.sleep (10)
-    # print("way to sms")
-
-# driver.quit()
+	response2=requests.get("http://www.kit19.com/ComposeSMS.aspx?username=id&password=password&sender=senderid&to=mobileno&message=Their%20is%20some%20technical%20issue%20in%20lottery%20admin%20pannel%20please%20check%20it%20immediately&priority=1&dnd=1&unicode=0")
+ 
